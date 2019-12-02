0fd99e70631b4595eaaef40116b7c9c6c5df6827
alexandre beelen <alexandre.beelen@ias.u-psud.fr>
2019-12-02 21:50:20 +0100

    After JG

--- day02/day02.py
+++ day02/day02.py
@@ -1,57 +1,47 @@
+from typing import List
 
-opcode = [1, 2, 99]
+Program = List[int]
 
-Intcode = [1,9,10,3,
-           2,3,11,0,
-           99,
-           30,40,50]
-
-def Intcode(code):
+def run(program: Program) -> Program:
     idx = 0
-    while idx < len(code):
-        opcode = code[idx]
+    while program[idx] != 99:
 
-        if opcode == 99:
-            return code
+        opcode = program[idx]
+        addr1, addr2, addr3 = program[idx+1], program[idx+2], program[idx+3]
 
-        if opcode not in [1, 2]:
-            return code
+        if opcode == 1:
+            program[addr3] = program[addr1] + program[addr2]
+        elif opcode == 2:
+            program[addr3] = program[addr1] * program[addr2]
+        else:
+            raise ValueError("Wrong opcode {}".format(opcode))
 
-        addr1, addr2, addr3 = [code[idx+i] for i in range(1, 4)]
-        mem1 = code[addr1]
-        mem2 = code[addr2]
-
-        if code[idx] == 1:
-            code[addr3] = mem1 + mem2
-        elif code[idx] == 2:
-            code[addr3] = mem1 * mem2
         idx += 4
-    return code
+    return program
 
-assert Intcode([1,0,0,0,99]) == [2,0,0,0,99]
-assert Intcode([2,3,0,3,99]) == [2,3,0,6,99]
-assert Intcode([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
-assert Intcode([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
+assert run([1,0,0,0,99]) == [2,0,0,0,99]
+assert run([2,3,0,3,99]) == [2,3,0,6,99]
+assert run([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
+assert run([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
 
 with open('input') as f:
-    codes = [int(code) for code in f.readline().strip().split(',')]
+    PROGRAM = [int(code) for code in f.readline().strip().split(',')]
 
-codes[1] = 12
-codes[2] = 2
-print(Intcode(codes)[0])
-
-def memory(noun, verb):
-    with open('input') as f:
-        codes = [int(code) for code in f.readline().strip().split(',')]
-
+def memory(PROGRAM, noun, verb):
+    codes = PROGRAM[:]
     codes[1] = noun
     codes[2] = verb
-    return Intcode(codes)[0]
+    return run(codes)[0]
+
+print(memory(PROGRAM, 12, 2))
 
 from itertools import product
 nouns = range(99)
 verbs = range(99)
 
+TARGET = 19690720
+
 for noun, verb in product(nouns, verbs):
-    if memory(noun, verb) == 19690720:
+    if memory(PROGRAM, noun, verb) == TARGET:
         print(100 * noun + verb)
+        break
