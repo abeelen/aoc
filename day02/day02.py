43fd527860a0da4663be3acb4dff56d842286ce6
alexandre beelen <alexandre.beelen@ias.u-psud.fr>
2020-12-02 21:20:05 +0100

    end day02

--- day02/day02.py
+++ day02/day02.py
@@ -1,1 +1,20 @@
+PASSWORD = """1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc"""
 
+import re
+
+def validate(line: str) -> bool:
+    cmin, cmax, char, password = re.match('(\d*)-(\d*) (.): (.*)', line).groups()
+    c = password.count(char)
+    return (int(cmin) <= c) & (c <= int(cmax))
+
+
+assert [validate(line.strip()) for line in PASSWORD.split('\n')] == [True, False, True]
+
+with open('day02.txt') as f:
+    validation = [validate(line.strip()) for line in f]
+
+print(sum(validation))
+
+## Part Two
