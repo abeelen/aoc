d812dbc4c6259587ade7c7f625392104d8f24e05
alexandre beelen <alexandre.beelen@ias.u-psud.fr>
2020-12-08 23:01:07 +0100

    correction

--- day08/day08.py
+++ day08/day08.py
@@ -1,35 +1,7 @@
-from typing import List
+from __future__ import annotations
+
+from typing import List, Set, NamedTuple
 from dataclasses import dataclass, field
-
-@dataclass
-class Isolation(object):
-    bootcode: List[str]
-    index: int = 0
-    accumulator: int = 0
-    visited: List[int] = field(default_factory=list[int])
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        self.visited.append(self.index)
-        instr, value = self.bootcode[self.index].split()
-
-        if instr == 'acc':
-            self.accumulator += int(value)
-            self.index += 1
-        elif instr == 'jmp':
-            self.index += int(value)
-        elif instr == 'nop':
-            self.index += 1
-        else:
-            raise ValueError('Unknown instruction')
-
-        if self.index == len(self.bootcode):
-            raise StopIteration
-        
-        if self.index in self.visited:
-            raise ValueError
 
 RAW="""nop +0
 acc +1
@@ -41,16 +13,59 @@ acc +1
 jmp -4
 acc +6"""
 
+class Instruction(NamedTuple):
+    op: str
+    arg: int
+
+    @staticmethod
+    def from_line(line: str) -> Instruction:
+        op, arg = line.strip().split()
+        return Instruction(op, int(arg))
+
+INSTRUCTIONS = [Instruction.from_line(line) for line in RAW.split('\n')]
+
+@dataclass
+class Isolation(object):
+    bootcode: List[Instruction]
+    index: int = 0
+    accumulator: int = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        op, value = self.bootcode[self.index]
+
+        if op == 'acc':
+            self.accumulator += value
+            self.index += 1
+        elif op == 'jmp':
+            self.index += value
+        elif op == 'nop':
+            self.index += 1
+        else:
+            raise ValueError(f'Unknown op{op}')
+
+        if self.index == len(self.bootcode):
+            raise StopIteration
+
+    def iterate_until_repeat(self):
+        visited = set()
+        self.index = 0
+        while self.index not in visited:
+            visited.add(self.index)
+            self.__next__()
+        return self.accumulator
+
+
+assert Isolation(INSTRUCTIONS).iterate_until_repeat() == 5
+
 with open('day09.txt') as f:
     raw = f.read().strip()
+    instructions = [Instruction.from_line(line) for line in raw.split('\n')]
+    prog = Isolation(instructions)
+    print(prog.iterate_until_repeat())
 
-prog = Isolation(raw.split('\n'))
-
-
-try:
-    _ = list(prog)
-except ValueError:
-    print(prog.accumulator)
 
 
 CORRECT="""nop +0
@@ -63,29 +78,24 @@ acc +1
 nop -4
 acc +6"""
 
-def correct_bootcode(bootcode: List[str]):
+def correct_bootcode(bootcode: List[Instruction]) -> int:
 
     # Brute force
-    index = 0
-    while index < len(bootcode):
-        instr, value = bootcode[index].split()
-        if instr not in ['jmp', 'nop']:
-            index +=1
-            continue
 
-        code = bootcode.copy()
-        if instr == 'jmp':
-            code[index] = f"nop {value}"
-        elif instr == 'nop':
-            code[index] = f'jmp {value}'
+    for i, (op, arg) in enumerate(bootcode):
+        code = bootcode[:]
+        if op == 'jmp':
+            code[i] = Instruction("nop", arg)
+        elif op == 'nop':
+            code[i] = Instruction('jmp', arg)
+        else:
+            continue
 
         prog = Isolation(code)
         try:
-            _ = list(prog)
-            break
-        except ValueError:
-            index += 1
-    
-    print(prog.accumulator)
+            prog.iterate_until_repeat()
+        except StopIteration:
+            return prog.accumulator
 
-correct_bootcode(raw.split('\n'))
+assert correct_bootcode(INSTRUCTIONS) == 8
+print(correct_bootcode(instructions))
